c1d614d394d35eef887055828d2334735c2fa575
zeyucui1 <zeyu.cui@student.uts.edu.au>
2023-09-16 16:44:59 +1000

    feat: add filter & sort function for cabin table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,29 +1,37 @@
-import styled from 'styled-components'
-
 import Spinner from '../../ui/Spinner'
 import CabinRow from './CabinRow'
 import { useCabins } from './useCabins'
 import Table from '../../ui/Table'
 import Menus from '../../ui/Menus'
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`
+import { useSearchParams } from 'react-router-dom'
 
 const CabinTable = () => {
   const { isLoading, cabins } = useCabins()
+  const [searchParams] = useSearchParams()
   if (isLoading) return <Spinner />
+  // filter function
+  const filterValue = searchParams.get('discount') || 'all'
+  let filteredCabins
+  if (filterValue === 'all') filteredCabins = cabins
+  if (filterValue === 'no-discount')
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0)
+  if (filterValue === 'with-discount')
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0)
+
+  //sort function
+  const sortBy = searchParams.get('sortBy') || 'name-asc'
+  const [field, direction] = sortBy.split('-')
+  const modifier = direction === 'asc' ? 1 : -1
+  function compareText(a, b) {
+    return a[field].localeCompare(b[field]) * modifier
+  }
+  function compareNumbers(a, b) {
+    return (a[field] - b[field]) * modifier
+  }
+  const sortedCabins =
+    typeof cabins[0][field] === 'number'
+      ? filteredCabins.sort(compareNumbers)
+      : filteredCabins.sort(compareText)
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -36,7 +44,7 @@ const CabinTable = () => {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={cabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
--- src/ui/Select.jsx
+++ src/ui/Select.jsx
@@ -1,15 +1,31 @@
-import styled from "styled-components";
+import { useSearchParams } from 'react-router-dom'
+import styled from 'styled-components'
 
 const StyledSelect = styled.select`
   font-size: 1.4rem;
   padding: 0.8rem 1.2rem;
   border: 1px solid
     ${(props) =>
-      props.type === "white"
-        ? "var(--color-grey-100)"
-        : "var(--color-grey-300)"};
+      props.type === 'white'
+        ? 'var(--color-grey-100)'
+        : 'var(--color-grey-300)'};
   border-radius: var(--border-radius-sm);
   background-color: var(--color-grey-0);
   font-weight: 500;
   box-shadow: var(--shadow-sm);
-`;
+`
+const Select = ({ options, value, onChange, ...props }) => {
+
+  return (
+    // 这里有个小trick 如果指向把props往下传可以用{...props}形式传参并且接收
+    <StyledSelect value={value} onChange={onChange} {...props}>
+      {options.map((option) => (
+        <option value={option.value} key={option.key}>
+          {option.label}
+        </option>
+      ))}
+    </StyledSelect>
+  )
+}
+
+export default Select
